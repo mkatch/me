b245d2b6d5749f1062853fd3ddce757eef9ddf5f
Marcin Kaczmarek <mkacz91@gmail.com>
2024-03-15 16:22:03 +0100

    Basic header

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,97 @@
 import './style.css'
+
+const headerBackgroundElement = document.getElementById('header-background')!;
+const navElement = document.getElementsByTagName('nav')[0]!;
+const nameElement = document.getElementById('name')!;
+
+const nameAnimation = [
+	'|',
+	'M|',
+	'Ma|',
+	'Mar|',
+	'Marc|',
+	'Marci|',
+	'Marcin|',
+	'Marcin |',
+	'Marcin K|',
+	'Marcin Ka|',
+	'Marcin Kac|',
+	'Marcin Kacz|',
+	'Marcin Kaczm|',
+	'Marcin Kaczma|',
+	'Marcin Kaczmar|',
+	'Marcin Kaczmare|',
+	'Marcin Kaczmarek|',
+	'Marcin Kaczmare|',
+	'Marcin Kaczmar|',
+	'Marcin Kaczma|',
+	'Marcin Kaczm|',
+	'Marcin Kacz|',
+	'Marcin Kac|z',
+	'Marcin Ka|cz',
+	'Marcin K|acz',
+	'Marcin |kacz',
+	'Marcin|kacz',
+	'Marci|kacz',
+	'Marc|kacz',
+	'Mar|kacz',
+	'Ma|kacz',
+	'M|kacz',
+	'mkacz',
+	// 'Marcin [|K]acz',
+	// 'Marcin [|K]acz',
+	// 'Marcin [|K]acz',
+	// '[|Marcin K]acz',
+	// '[|Marcin K]acz',
+	// '[|Marcin K]acz',
+	// 'm|acz',
+	// 'mk|acz',
+	// 'Marcin k|acz',
+	// 'Marcin |kacz',
+	// 'Marcin|kacz',
+	// 'Marci|kacz',
+	// 'Marc|kacz',
+	// 'Mar|kacz',
+	// 'Ma|kacz',
+	// 'M|kacz',
+	// '|>M<kacz',
+	// 'm|kacz',
+	// '|mkacz',
+];
+
+const revealAnimationLastFrameIndex = nameAnimation.indexOf('Marcin Kaczmarek|');
+let nameAnimationFrameIndex = -1;
+function setNameAnimationFrame(frameIndex: number) {
+	if (nameAnimationFrameIndex === frameIndex) {
+		return;
+	}
+	const gg = nameAnimation[frameIndex]
+		.replace(/\[/, '<span class="select">')
+		.replace(/\]/, '</span>')
+		.replace(/\|(.)/, '<span class="cursor">$1</span>')
+		.replace(/\|$/, '<span class="cursor">&nbsp;</span>');
+	nameElement.innerHTML = gg;
+	nameAnimationFrameIndex = frameIndex;
+}
+
+const revealAnimationTimer = setInterval(() => {
+	if (nameAnimationFrameIndex >= revealAnimationLastFrameIndex) {
+		clearInterval(revealAnimationTimer);
+		return;
+	}
+	setNameAnimationFrame(nameAnimationFrameIndex + 1);
+}, 60);
+
+function onAnimationFrame() {
+	const scrollTop = document.scrollingElement!.scrollTop;
+	const tCollapse = Math.max(0, Math.min(1, scrollTop / (headerBackgroundElement.offsetHeight - navElement.offsetHeight)));
+	document.body.style.setProperty('--scroll-top', CSS.px(scrollTop).toString());
+	document.body.style.setProperty('--t-collapse', tCollapse.toString());
+
+	const t = tCollapse;
+	const i = Math.round(revealAnimationLastFrameIndex + t * (nameAnimation.length - 1 - revealAnimationLastFrameIndex));
+	setNameAnimationFrame(i);
+
+	window.requestAnimationFrame(onAnimationFrame);
+};
+window.requestAnimationFrame(onAnimationFrame);
